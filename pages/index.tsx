634dd7d36fa6e6d48fb7b2dd3ffb0ed59eb46d75
ybandla <bandla.yash@gmail.com>
2025-10-04 17:46:17 -0500

    updated folder structure

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Homepage</h1>
+      <p>This is a basic Next.js + TypeScript homepage.</p>
+    </div>
+  );
+}
